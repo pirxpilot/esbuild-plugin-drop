6f305a60f03446e67c391ec04eac2873442ab9bd
Damian Krzeminski <pirxpilot@furkot.com>
2024-01-03 21:47:37 +0100

    add support for droping imports

--- test/esbuild-plugin-drop.js
+++ test/esbuild-plugin-drop.js
@@ -5,7 +5,7 @@ const { readFile } = require('fs').promises;
 
 const drop = require('../');
 
-test('must drop assert', async function (t) {
+test('must drop assert require', async function (t) {
   const sourceName = resolve(__dirname, 'fixtures', 'assert.js');
   const expectedName = resolve(__dirname, 'fixtures', 'assert.esbuild.js');
 
@@ -21,9 +21,25 @@ test('must drop assert', async function (t) {
   t.end();
 });
 
-test('must drop assert (esm)', async function (t) {
+test('must drop assert import', async function (t) {
   const sourceName = resolve(__dirname, 'fixtures', 'assert.mjs');
   const expectedName = resolve(__dirname, 'fixtures', 'assert.esbuild.mjs');
+
+  const [
+    transformed,
+    expected,
+  ] = await Promise.all([
+    build(sourceName, {}, { sourceType: 'module' }),
+    readFile(expectedName, 'utf-8')
+  ]);
+
+  t.equal(transformed, expected);
+  t.end();
+});
+
+test('must drop assert require if source type is module', async function (t) {
+  const sourceName = resolve(__dirname, 'fixtures', 'assert.js');
+  const expectedName = resolve(__dirname, 'fixtures', 'assert.esbuild.js');
 
   const [
     transformed,
